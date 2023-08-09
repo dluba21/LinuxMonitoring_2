18929668207d17451820277293324fb414a73f93
dluba <milpz1c@gmail.com>
2023-08-10 00:43:17 +0300

    second save

--- src/01/01.sh
+++ src/01/01.sh
@@ -5,6 +5,18 @@ function atoi() {
      printf '%d' "'$1'";
 }
 
+function clear_file() {
+     echo -n "" > $1;
+}
+
+function get_free_space() {
+    echo $(df -m / | awk '{print $4}' | sed -n '2p');
+}
+
+function get_creation_time() {
+    echo $(stat -t "%d-%m-%Y %H:%m:%S" $1 | awk '{print $9, $10}' | tr -d \");
+}
+
 function dir_name_validator() {
     if [[ ! $dir_letters =~ ^[A-Za-z]{1,7}$ ]]; then
         echo "ERROR: file name [$dir_letters] has not just chars";
@@ -17,6 +29,19 @@ function file_name_validator() {
         echo "ERROR: file name [$file_letters] doesn't match pattern *{1,7}.*{1,3}*";
         exit -1;
     fi;
+}
+
+function get_dot_position() {
+    local string=$1;
+
+    for (( i = 0; i < ${#string}; i++ )); do
+        if [[ ${string:i:1} == "." ]]; then
+            echo $i;
+            return 0;
+        fi;
+    done;
+    echo "ERROR: dot in filename wasn't found";
+    exit -1;
 }
 
 
@@ -57,6 +82,10 @@ function arg_validator() {
 }
 
 
+
+
+
+
 # $1 - нижняя граница $2 - верхняя
 function randnum() {
     echo $(shuf -i $1-$2 -n 1);
@@ -69,7 +98,7 @@ function get_rand_name() {
 
     for (( i = 0; i < ${#letters}; i++ )); do 
         local letter=${letters:i:1};
-        local num_of_letters=$(randnum 1 10);
+        local num_of_letters=$(randnum 1 3);
         for (( j = 0; j < $num_of_letters; j++ )); do
             word+=$letter;
         done;
@@ -80,10 +109,10 @@ function get_rand_name() {
 
 function get_filename_part() {
     local file_name=$1;
-    local dot_position=$(echo $file_name | grep -aob '.' | grep -oE '[0-9]+');
-    if [[ $2 -eq "FIRST_PART" ]]; then
+    local dot_position=$(get_dot_position $file_name);
+    if [[ $2 == "FIRST_PART" ]]; then
         echo ${file_name:0:$dot_position};
-    elif [[ $2 -eq "SECOND_PART" ]]; then
+    elif [[ $2 == "SECOND_PART" ]]; then
         echo ${file_name:$(expr $dot_position + 1):${#file_name}};
     else
         echo "ERROR: incorrect input for \"get_filename_part\" function";
@@ -93,131 +122,102 @@ function get_filename_part() {
 
 
 function file_name_composer() {
-    local string=$(get_rand_name $(get_filename_part "FIRST_PART" $file_letters));
+    local string=$(get_rand_name $(get_filename_part $file_letters "FIRST_PART"));
     string+=".";
-    string+=$(get_rand_name $(get_filename_part "SECOND_PART" $file_letters));
-    string+="_$(date +%d%m%Y)";
+    string+=$(get_rand_name $(get_filename_part $file_letters "SECOND_PART"));
+    string+="_$(date +%d%m%y)";
     echo $string;
 }
 
 function dir_name_composer() {
     local string=$(get_rand_name $dir_letters);
-    string+="_$(date +%d%m%Y)";
+    string+="_$(date +%d%m%y)";
     echo $string;
 }
 
 
 
-function get_size_each_file() {
-    
-}
 
 function file_generator() {
-    if [[ ! -d abs_path ]]; then
-        echo "ERROR: $abs_path doesn't exist";
-        exit -1;
-    fi;
-    # local free_space=$(df -m / | awk '{print $4}' | sed -n '2p');
-    touch ${abs_path}/file_log;
+    # починить условие валидации
+    # if [[ ! -d abs_path ]]; then
+    #     echo "ERROR: $abs_path doesn't exist";
+    #     exit -1;
+    # fi;
+   
+
     local file_log_path=${abs_path}/file_log;
-    touch ${abs_path}/dir_log;
+    touch $file_log_path;
     local dir_log_path=${abs_path}/dir_log;
+    touch $dir_log_path;
+    # clear_file $file_log_path;
+    # clear_file $dir_log_path;
+    
+    local current_dir=$abs_path;
+    cd $current_dir;
 
-    local dir_name=$(name_composer $dir_letters);
-    local file_name_main=$(name_composer $(get_filename_part "FIRST_PART" $file_letters));
-    local file_name_ext=$(name_composer $(get_filename_part "SECOND_PART" $file_letters));
-
-    cd $abs_path;
+    local current_dir="";
+    local current_file="";
+    local creation_time="";
     for (( i = 0; i < dir_number; i++ )); do
-        mkdir 
+        current_dir=$(dir_name_composer);
+        mkdir $current_dir;
+        creation_time=$(get_creation_time $current_dir);
+        cd $current_dir;
+        printf '%s %s\n' $(pwd) $creation_time >> $dir_log_path;
+
+        for (( j = 0;  $mem_lower_limit <= $(get_free_space) && j < $files_number; j++ )) {
+            current_file=$(file_name_composer);
+            truncate -s "${file_size}K" $current_file;
+            printf '%s %s %s\n' "$current_dir/$current_file" "$(get_creation_time $current_file)" "$file_size" >> $file_log_path;
+        }
     done;
-
-
-
-
 }
 
 
 arg_length=$#
-abs_path=$1;
-dir_number=$2;
-dir_letters=$3;
-files_number=$4;
-file_letters=$5;
-file_size=$6;
+abs_path="/Users/anrdeysuvorov/projects/LinuxMonitoring_2/src/01/test";
+dir_number=30;
+dir_letters="abcdef";
+files_number=10;
+file_letters="abcdef.ghi";
+file_size=100; #в кб
 
-# echo $abs_path
-# echo 
-# arg_validator;
-# atoi "b";
-# if [[ "a" == "a" && "p" == "b" || "c" == "c"  ]]; then
-# echo "true";
-# fi;
+mem_lower_limit=29700 #в Мб
 
-# file_name_validator;
 
-# printf '%c' 112;
+# function file_name_composer() {
+#     local string=$(get_rand_name $(get_filename_part "FIRST_PART" $file_letters));
+#     string+=".";
+#     string+=$(get_rand_name $(get_filename_part "SECOND_PART" $file_letters));
+#     string+="_$(date +%d%m%Y)";
+#     echo $string;
+# }
 
-# char=$(atoi "a");
-# char="A";
-# echo $(expr "97" + 0 )
+# file_name_composer;
+# get_rand_name "aavvgg";
 
-# echo "A = " $(atoi "A") "Z = " $(atoi "Z") "a = " $(atoi "a") "z = " $(atoi "z") "var = " $char;
+# echo $abs_path;
 
-# if [[ $char -lt $(atoi "A") ||  $(atoi $char) -gt $(atoi "Z") && $(atoi $char) -lt $(atoi "a") || $(atoi $char) -gt $(atoi "z") ]]; then   
-# echo "false";
-# fi;
+file_generator;
 
-# char="a";
 
-# if [[ $char < $(atoi "A") || $(atoi $char) > $(atoi "Z") || $(atoi $char) -gt $(atoi "Z") && $(atoi $char) -lt $(atoi "a") ]]; then
-# echo "false";
-# fi;
+# get_dot_position $file_letters;
 
-# if [[ $(atoi $char) -gt $(atoi "Z") && $(atoi $char) -lt $(atoi "a") || $char -lt $(atoi "A") || $char -gt $(atoi "z") ]]; then
-# echo "false";
-# fi;
+# function get_dot_position() {
+#     local string=$1;
 
-# if [[ ascii_code < 65 || (ascii_code > 90 && ascii_code < 97) || ascii_code > 122 ]]; then
-#     echo "Символ $character не является латинской буквой."
-# else
-#     echo "Символ $character является латинской буквой."
-# fi;
+#     for (( i = 0; i < ${#string}; i++ )); do
+#         if [ ${string:i:1} == "." ]; then
+#             echo $i;
+#             break;
+#         fi;
+#         echo ${string:i:1};
+#     done;
+#     echo "ERROR: dot in filename wasn't found";
+#     exit -1;
+# }
 
-# if [[ ! "$character" =~ ^[a-zA-Z]+$ ]]; then
-#     echo "Символ $character yes"
-# else
-#     echo "Символ $character no"
-# fi
-
-string="aazziia";
-n=14;
-var="";
-dot_position=3;
-var1=${string:0:$dot_position};
-
-#  if [[ ${#var1} < 7 ]]; then
-# echo "true"
-#     fi;
-
-# if [[ ${#var1} < 7 ]]; then
-#     echo "true"
-# fi;
-
-# echo $(${#string} / 2 != 0);
-
-# ${#dir_letters};
-
-var="aaa.aa";
-
-# if [[ $var =~ ^[a-zA-Z]{1,7}\.[a-zA-Z]{1,3}$ ]]; then
-#     echo "nice";
-# fi;
-
-dir_letters="asv.rav";
-#  if [[ ! $dir_letters =~ ^[A-Za-z]{1,7}\.[a-zA-Z]{1,3}$ ]]; then
-#         echo "ERROR: file name [$dir_letters] doesn't match pattern *{1,7}.*{1,3}*";
-#         exit -1;
-#     fi;
-
-file_name_composer;
+# get_dot_position $file_letters;
+# file_name_composer;
+# dir_name_composer;
